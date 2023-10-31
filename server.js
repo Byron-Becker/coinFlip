7afce3b9e5ab18088e286f0b275992ee76555c11
bbeck9123 <byron.ptgo@gmail.com>
2023-10-31 07:46:02 -0700

    testing gitignore.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const PORT = 3000
 const MongoClient = require('mongodb').MongoClient
 
+MongoClient.connect()
+
 app.listen(PORT, (req,res) => {
     console.log('Connected on Port 3000')
 })
