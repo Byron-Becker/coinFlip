1b604c903365253570fdb1ef96f61eb2b1c8e5ec
bbeck9123 <byron.ptgo@gmail.com>
2023-11-12 15:55:56 -0800

    yo

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.listen(PORT, (req,res) => {
 // operations to many different types of data
 // allow for improved abstraction because they do not require the need for intermediate variables or explicit loops or control structures
 
+// textContent is preferred because it gives it raw
 
+const func = (str,c) => (str.match(new RegExp(c, 'ig')||[])).length
 
 
 
